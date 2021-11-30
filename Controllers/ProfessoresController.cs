221369c5a460fc57d022e3809069fdc3d576816f
Diego Barbosa <diegobarbosarocha@gmail.com>
2021-11-30 19:30:09 -0300

    Get specific and put Professor working

--- Controllers/ProfessoresController.cs
+++ Controllers/ProfessoresController.cs
@@ -16,8 +16,30 @@ namespace CoursesAPI.Controllers
         //GET       api/professores
         
         [HttpGet]
-        public ActionResult<IEnumerable<Professor>> GetProfessores(){
+        public ActionResult<IEnumerable<Professor>> GetProfessores()
+        {
             return _context.Professores;
+        }
+
+        //GET       api/professores/id
+        [HttpGet("{id}")]
+        public ActionResult<Professor> GetProfessor(int id)
+        {
+            var prof = _context.Professores.Find(id);
+            if(prof == null)
+            {
+                return NotFound();
+            }
+            return prof;
+        }
+
+        //POST      api/professores
+        [HttpPost]
+        public ActionResult<Professor> CreateProfessor(Professor prof)
+        {
+            _context.Professores.Add(prof);
+            _context.SaveChanges();
+            return CreatedAtAction("GetProfessor",new Professor{Id = prof.Id}, prof);
         }
     }
 }
